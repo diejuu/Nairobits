1d8bf12d586217678983aaf10dd8bf301dc7331d
diejuu <hannahwanjiku510@gmail.com>
2018-05-19 12:54:41 -0700

    add calculation

---
+++ testr.py
@@ -0,0 +1,12 @@
+num1 = 6
+num2 = 7
+num3 = 12
+
+calc ={}
+sum =num1+num2
+product=num2*num3
+minus= num3-num1
+
+calc["addition"] =sum
+calc["subtraction"] =minus
+calc["multiplication"] = product
